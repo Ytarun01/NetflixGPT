9f9bef83ea3c4aa670961954e1b0e705590d06a2
Tarun <raotarun98111@gmail.com>
2024-07-24 01:27:35 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Body from "./components/Body";
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Body />
+    </>
+  );
 }
 
 export default App;
