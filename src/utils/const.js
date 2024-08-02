f1873568952798f26b7939648f2425a174dfeb9c
Tarun <raotarun98111@gmail.com>
2024-08-02 23:41:39 +0530

    resolved api error

--- src/utils/const.js
+++ src/utils/const.js
@@ -2,8 +2,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZGYxM2Q3Njg5ZjJlOTkxYTlkMTY1OWE1Nzk1NmQ5NiIsIm5iZiI6MTcyMjAxNzczMC4zMDk1MTcsInN1YiI6IjY2YTNjMzk2MjdjNDI0YTg4OTgxMTU5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.786JBCb4EgTsDTOF_2Fv4QvUjqCEHstU1AQdr0EQ-EY",
+    Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
   },
 };
 
