8f8e2759901288148619084372e942b2c342c543
Tarun <raotarun98111@gmail.com>
2024-08-02 21:26:06 +0530

    updated gpt

--- src/utils/const.js
+++ src/utils/const.js
@@ -12,5 +12,5 @@ export const IMG_URL = "https://image.tmdb.org/t/p/w200";
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
-// export const OPENAIKEY =
-//   "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";
+export const OPENAIKEY =
+  "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";
