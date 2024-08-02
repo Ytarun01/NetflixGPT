51f583e43c9ebe6de23fe963e4ab7d43d4da448f
Tarun <raotarun98111@gmail.com>
2024-08-02 23:37:49 +0530

    solve openapi error

--- src/utils/const.js
+++ src/utils/const.js
@@ -12,5 +12,4 @@ export const IMG_URL = "https://image.tmdb.org/t/p/w200";
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
-export const OPENAIKEY =
-  "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";
+export const OPENAIKEY = process.env.REACT_APP_OPENAI_API_KEY;
