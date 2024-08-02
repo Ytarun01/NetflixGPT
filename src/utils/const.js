f975705b9e1963afeb0770395a5fd1787dd94142
Tarun <raotarun98111@gmail.com>
2024-08-02 23:47:13 +0530

    error resolved

--- src/utils/const.js
+++ src/utils/const.js
@@ -2,7 +2,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_KEY}`,
   },
 };
 
